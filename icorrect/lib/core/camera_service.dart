70f83c644f3cfbe375d1280355b692f09f35add8
sonnm2023 <gotoau2023@gmail.com>
2023-10-09 09:45:15 +0700

    no message

--- icorrect/lib/core/camera_service.dart
+++ icorrect/lib/core/camera_service.dart
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-// import 'package:camera/camera.dart';
-// import 'package:flutter/foundation.dart';
-//
-// class CameraService {
-//   CameraController? _cameraController;
-//   List<CameraDescription>? _cameras;
-//   CameraController? get cameraController => _cameraController;
-//
-//   Future<void> initialize(Function refreshState) async {
-//     _cameras = await availableCameras();
-//
-//     _cameraController = CameraController(
-//       _cameras![1],
-//       ResolutionPreset.low,
-//       enableAudio: true,
-//     );
-//
-//     await _cameraController!.initialize().then((value) {
-//       refreshState();
-//     }).catchError((e) {
-//       print(e);
-//     });
-//   }
-//
-//   void startCameraRecording() {
-//     if (_cameraController != null) {
-//       _cameraController!.startVideoRecording();
-//     }
-//   }
-//
-//   void saveVideoDoingTest() {
-//     _cameraController!.stopVideoRecording().then((value) async {
-//       if (value != null) {
-//         if (kDebugMode) {
-//           int length = (await value.readAsBytes()).lengthInBytes;
-//           print(
-//               "DEBUG : Video Recording saved to ${value.path}, size : ${length / 1024}kb, size ${(length / 1024) / 1024}mb");
-//         }
-//       }
-//     });
-//   }
-//
-//   dispose() async {
-//     await _cameraController?.dispose();
-//     _cameraController = null;
-//   }
-// }
-=======
 import 'dart:io';
 
 import 'package:camera/camera.dart';
@@ -83,13 +34,11 @@ class CameraService {
   void saveVideoDoingTest(Function(File savedFile) saveVideoCallBack) {
     try {
       _cameraController!.stopVideoRecording().then((value) async {
-        if (value != null) {
-          saveVideoCallBack(File(value.path));
-          if (kDebugMode) {
-            int length = (await value.readAsBytes()).lengthInBytes;
-            print(
-                "DEBUG : Video Recording saved to ${value.path}, size : ${length / 1024}kb, size ${(length / 1024) / 1024}mb");
-          }
+        saveVideoCallBack(File(value.path));
+        if (kDebugMode) {
+          int length = (await value.readAsBytes()).lengthInBytes;
+          print(
+              "DEBUG : Video Recording saved to ${value.path}, size : ${length / 1024}kb, size ${(length / 1024) / 1024}mb");
         }
       });
     } catch (e) {
@@ -106,4 +55,3 @@ class CameraService {
     }
   }
 }
->>>>>>> build/update_exam_logic
