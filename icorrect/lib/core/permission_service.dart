c6bf80c62fdacd01eb80e2f0f92058da634c6e71
sonnm2023 <gotoau2023@gmail.com>
2023-10-25 14:34:42 +0700

    no message

--- icorrect/lib/core/permission_service.dart
+++ icorrect/lib/core/permission_service.dart
@@ -11,33 +11,35 @@ class PermissionHandler {
   }
 }
 
-// class PermissionService{
-//   final PermissionHandler _permissionHandler = PermissionHandler();
-//
-//   Future<bool> _requestPermission() async {
-//     var result = await _permissionHandler.requestPermissions([Permission.microphone, Permission.camera]);
-//     // ignore: unrelated_type_equality_checks
-//     if (result == PermissionStatus.granted) {
-//       return true;
-//     }
-//     return false;
-//   }
-//
-//   Future<bool> requestPermission({required Function onPermissionDenied}) async {
-//     var granted = await _requestPermission();
-//     if (!granted) {
-//       onPermissionDenied();
-//     }
-//     return granted;
-//   }
-//
-//   Future<bool> hasPhonePermission() async {
-//     return hasPermission(Permission.phone);
-//   }
-//
-//   Future<bool> hasPermission(Permission permission) async {
-//     var permissionStatus =
-//     await _permissionHandler.checkPermissionStatus(permission);
-//     return permissionStatus == PermissionStatus.granted;
-//   }
-// }
+class PermissionService{
+  final PermissionHandler _permissionHandler = PermissionHandler();
+
+  Future<bool> _requestPermission() async {
+    var result = await _permissionHandler.requestPermissions([Permission.microphone, Permission.camera]);
+    // ignore: unrelated_type_equality_checks
+    if (result == PermissionStatus.granted) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> requestPermission({required Function onPermissionDenied}) async {
+    var granted = await _requestPermission();
+    if (!granted) {
+      onPermissionDenied();
+    }
+    return granted;
+  }
+
+  Future<bool> hasCameraPermission() async {
+    return hasPermission(Permission.camera);
+  }
+
+  Future<bool> hasMicrophonePermission() async {
+    return hasPermission(Permission.microphone);
+  }
+
+  Future<bool> hasPermission(Permission permission) async {
+    return _permissionHandler.isPermissionGranted(permission.status as PermissionStatus);
+  }
+}
