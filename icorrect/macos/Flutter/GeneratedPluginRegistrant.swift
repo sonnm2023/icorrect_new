5f79e5a1de61811e8045e613f1f1fb58ed6aa982
sonnm2023 <gotoau2023@gmail.com>
2023-10-26 14:49:41 +0700

    no message

--- icorrect/macos/Flutter/GeneratedPluginRegistrant.swift
+++ icorrect/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,13 +9,13 @@ import audioplayers_darwin
 import connectivity_plus
 import device_info_plus
 import flutter_localization
-import light_compressor
 import package_info_plus
 import path_provider_foundation
 import record_macos
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
+import video_compress
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -23,12 +23,12 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
-  LightCompressorPlugin.register(with: registry.registrar(forPlugin: "LightCompressorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
