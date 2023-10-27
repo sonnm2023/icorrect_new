dfdf0e2711bb31f8ee99085a568bd43e1d04225a
sonnm2023 <gotoau2023@gmail.com>
2023-10-27 15:16:29 +0700

    Fix sending log for iOS version

--- icorrect/ios/Runner/AppDelegate.swift
+++ icorrect/ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import BackgroundTasks
 import Flutter
-import workmanager
 import Foundation
 
 @UIApplicationMain
@@ -10,7 +9,7 @@ import Foundation
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+      GeneratedPluginRegistrant.register(with: self)
     
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
     let nativeChannel = FlutterMethodChannel(name: "nativeChannel",
@@ -22,9 +21,15 @@ import Foundation
                  let apiUrl = args["api_url"] as? String,
                  let secretkey = args["secretkey"] as? String,
                  let filePath = args["file_path"] as? String {
-                  print("DEBUG: Native - send log: url = \(apiUrl) secretkey = \(secretkey)")
-                  print("DEBUG: Native - send log: file path = \(filePath)")
-//                  self.sendLog(apiUrl: apiUrl, secretkey: secretkey, filePath: filePath)
+                  debugPrint("DEBUG: Native - send log: url = \(apiUrl) secretkey = \(secretkey)")
+                  debugPrint("DEBUG: Native - send log: file path = \(filePath)")
+                  self.sendLog(apiUrl: apiUrl, filePath: filePath, secretkey: secretkey) { _ in
+                      debugPrint("DEBUG: Send log success!")
+                      self.deleteFile(filePath: filePath)
+                  } failure: {
+                      debugPrint("DEBUG: Send log error!")
+                  }
+                  
                   result(nil)
               } else {
                   result(FlutterError.init(code: "error", message: "data or format error", details: nil))
@@ -34,53 +39,49 @@ import Foundation
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     
-
-    private func sendLog(apiUrl: String, secretkey: String, filePath: String) {
-        DispatchQueue.background(background: {
-            // do something in background
-            print("DEBUG: SEND LOG")
-            let uploadURL = URL(string: apiUrl)!
-            var request = URLRequest(url: uploadURL)
-            request.httpMethod = "POST"
-            request.addValue(secretkey, forHTTPHeaderField: "secretkey")
-            request.addValue("flutter_logs.txt", forHTTPHeaderField: "file")
-
+    private func sendLog (
+        apiUrl: String,
+        filePath: String,
+        secretkey: String,
+        success: @escaping (Bool) -> Void,
+        failure: @escaping () -> Void) {
             if let fileURL: URL? = URL(fileURLWithPath: filePath) {
                 if (fileURL == nil) { return }
-                
                 do {
-                    let content = try String(contentsOf: fileURL!, encoding: .utf8)
-                    print("DEBUG: send log content: \(content)")
-                } catch (_) {
-                    print("DEBUG: send log check file error")
-                }
-                
-                    
-                let task = URLSession.shared.uploadTask(with: request, fromFile: fileURL!) { data, response, error in
-                    if let error = error {
-                        print("DEBUG: send log Lỗi khi tải lên: \(error)")
-                        return
-                    }
-                    
-                    if let httpResponse = response as? HTTPURLResponse {
-                        if httpResponse.statusCode == 200 {
-                            // Tải lên thành công
-                            print("DEBUG: send log Tải lên thành công")
+                    let content = try? String(contentsOf: fileURL!, encoding: .utf8)
+                    let param: [String : Any] = ["secretkey": secretkey, "file": content]
+                    NetworkManager.shared.callingHttpPostMethodWithFile(params: param, apiname: apiUrl, filePath: filePath, success: { [weak self](data) in
+                        guard self != nil else {return}
+                        if data == nil {
+                            success(true)
                         } else {
-                            print("DEBUG: send log Lỗi HTTP: \(httpResponse.statusCode)")
+                            success(false)
                         }
-                    }
+                    }, failure: { (error) in
+                        debugPrint("DEBUG: sendLog api error \(error?.localizedDescription ?? "sendLog api error")")
+                        failure()
+                    })
                 }
-                
-                task.resume()
-            } else {
-                print("DEBUG: send log: Không có file log")
-                return
             }
-        }, completion:{
-            // when background job finished, do something in main thread
-            print("DEBUG: send log: when background job finished, do something in main thread")
-        })
+        }
+    
+    private func deleteFile(filePath: String) {
+        do {
+             let fileManager = FileManager.default
+            
+            // Check if file exists
+            if fileManager.fileExists(atPath: filePath) {
+                debugPrint("DEBUG: DELETE log file")
+                // Delete file
+                try fileManager.removeItem(atPath: filePath)
+            } else {
+                debugPrint("DEBUG: File does not exist")
+            }
+         
+        }
+        catch let error as NSError {
+            debugPrint("DEBUG: An error took place: \(error)")
+        }
     }
 }
 
---
+++ icorrect/ios/Runner/NetworkManager.swift
@@ -0,0 +1,128 @@
+//
+//  NetworkManager.swift
+//  Runner
+//
+//  Created by Apple on 27/10/2023.
+//
+
+import Foundation
+import AFNetworking
+import UniformTypeIdentifiers
+
+class NetworkManager: NSObject {
+    static let shared = NetworkManager()
+    static let TIMEOUT = Double(60)
+    let manager = AFHTTPSessionManager()
+    
+    func callingHttpPostMethodWithFile (params:[String:Any]?,
+                                apiname : String,
+                                filePath: String,
+                                success: @escaping (_ result: Any?) -> Void,
+                                failure: @escaping (_ err: Error?) -> Void){
+        let urlString = apiname
+        let manager = AFHTTPSessionManager()
+        
+        manager.requestSerializer.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        manager.requestSerializer.timeoutInterval = NetworkManager.TIMEOUT
+        manager.requestSerializer.cachePolicy = .reloadIgnoringCacheData
+        
+        if let fileURL: URL? = URL(fileURLWithPath: filePath) {
+            if (fileURL == nil) { return }
+            
+            do {
+                let fileData = try? Data(contentsOf: fileURL!)
+                var data = Data()
+                data.append(fileData!)
+                manager.requestSerializer.setValue(String(data.count), forHTTPHeaderField: "Content-Length")
+                
+                var paramsOS = params
+                paramsOS?.updateValue("ios_flutter", forKey: "os")
+                
+                manager.post(urlString, parameters: paramsOS) { formData in
+                    formData.appendPart(withFileData: data, name: "file", fileName: "flutter_logs.txt", mimeType: "text/plain")
+                } progress: { Progress in
+                
+                } success: { (session, data) in
+                    if let response = (data as? [String: Any]) {
+                        debugPrint("DEBUG: Send log response ===== \(String(describing: response))")
+                        debugPrint("Status: \(String(describing: response["status"])) Message: \(String(describing: response["message"]))")
+//                        Common.deleteFile(fileName: "flutter_logs.txt")
+                        //TODO: Delete log file here
+                        success(response)
+                        URLCache.shared.removeCachedResponses(since: Date())
+                    }
+                } failure: { (session, error) in
+                    failure(error)
+                    
+                    if error.code == 401 {
+                        debugPrint("DEBUG: Send log error code: 401 - Needs refresh token")
+                    } else if error.code == 302 {
+                        debugPrint("test12\(String(describing: error.data))")
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension Error {
+    var code: Int { return self.userInfo["com.alamofire.serialization.response.error.response"] != nil ? (self.userInfo["com.alamofire.serialization.response.error.response"] as! HTTPURLResponse).statusCode : 500}
+    var domain: String { return (self as NSError).domain }
+    var userInfo: [String:Any] { return (self as NSError).userInfo }
+    var data:  [String:Any] { return self.userInfo["com.alamofire.serialization.response.error.response.data"] != nil ? (self.userInfo["com.alamofire.serialization.response.error.response.data"] as!  [String:Any]) : [:]}
+}
+
+extension NSURL {
+    public func mimeType() -> String {
+        if #available(iOS 14.0, *) {
+            if let pathExt = self.pathExtension,
+               let mimeType = UTType(filenameExtension: pathExt)?.preferredMIMEType {
+                return mimeType
+            }
+            else {
+                return "application/octet-stream"
+            }
+        } else {
+            // Fallback on earlier versions
+            return "application/octet-stream"
+        }
+    }
+}
+
+extension URL {
+    public func mimeType() -> String {
+        if #available(iOS 14.0, *) {
+            if let mimeType = UTType(filenameExtension: self.pathExtension)?.preferredMIMEType {
+                return mimeType
+            }
+            else {
+                return "application/octet-stream"
+            }
+        } else {
+            // Fallback on earlier versions
+            return "application/octet-stream"
+        }
+    }
+}
+
+extension NSString {
+    public func mimeType() -> String {
+        if #available(iOS 14.0, *) {
+            if let mimeType = UTType(filenameExtension: self.pathExtension)?.preferredMIMEType {
+                return mimeType
+            }
+            else {
+                return "application/octet-stream"
+            }
+        } else {
+            // Fallback on earlier versions
+            return "application/octet-stream"
+        }
+    }
+}
+
+extension String {
+    public func mimeType() -> String {
+        return (self as NSString).mimeType()
+    }
+}
