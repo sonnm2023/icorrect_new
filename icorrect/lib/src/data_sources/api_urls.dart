1a87f353c5e836be80df3147a149f34b28b95a4d
sonnm2023 <gotoau2023@gmail.com>
2023-11-04 11:26:48 +0700

    Change domain

--- icorrect/lib/src/data_sources/api_urls.dart
+++ icorrect/lib/src/data_sources/api_urls.dart
@@ -3,11 +3,15 @@
 
 import 'package:icorrect/src/data_sources/utils.dart';
 
-const apiDomain = "http://api.ielts-correction.com/";
+// const apiDomain = "http://api.ielts-correction.com/";
+// const icorrectDomain1 = "ielts-correction.com";
+// const publicDomain = "http://public.icorrect.vn/";
+// const toolDomain = "http://tool.ielts-correction.com/";
 const icorrectDomain = "https://ielts-correction.com/";
-const icorrectDomain1 = "ielts-correction.com";
-const publicDomain = "http://public.icorrect.vn/";
-const toolDomain = "http://tool.ielts-correction.com/";
+const publicDomain = icorrectDomain;
+const toolDomain = icorrectDomain;
+const apiDomain = icorrectDomain;
+
 
 ///// api endpoints
 const String registerEP = 'auth/register';
