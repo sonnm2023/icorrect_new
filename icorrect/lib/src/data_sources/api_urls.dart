c41bcd783bcac0ccb2a9da5ce51e53c6e81a956c
NguyenLuan1010 <vanluan101002@gmail.com>
2023-11-21 17:13:13 +0700

    update url

--- icorrect/lib/src/data_sources/api_urls.dart
+++ icorrect/lib/src/data_sources/api_urls.dart
@@ -7,11 +7,14 @@ import 'package:icorrect/src/data_sources/utils.dart';
 // const icorrectDomain1 = "ielts-correction.com";
 // const publicDomain = "http://public.icorrect.vn/";
 // const toolDomain = "http://tool.ielts-correction.com/";
-const icorrectDomain = "http://devapi.ielts-correction.com/";
-const publicDomain = "http://devpublic.icorrect.vn/";
-const toolDomain = "http://devtool.ielts-correction.com/";
-const apiDomain = "http://api.ielts-correction.com/";
-const testPositionApi = "http://devpublic.icorrect.vn/api/student/test-position";
+const icorrectDomain = "https://ielts-correction.com/";
+const publicDomain = icorrectDomain;
+const toolDomain = icorrectDomain;
+const apiDomain = icorrectDomain;
+const oldPublicDomain = "http://public.icorrect.vn/";
+const oldToolDomain = "http://tool.ielts-correction.com/";
+
+const testPositionApi = "http://public.icorrect.vn/api/student/test-position";
 const testPositionUser = "ic_landing";
 const testPositionPass = "5]N;3e:t<3uvgR2L";
 
@@ -29,14 +32,14 @@ String downloadFileEP(String name) => '${apiDomain}file?filename=$name';
 String fileEP(String name) => '${icorrectDomain}file?filename=$name';
 
 String responseEP(String orderId) =>
-    '${toolDomain}api/response?order_id=$orderId';
+    '${oldToolDomain}api/response?order_id=$orderId';
 
 Future<String> aiResponseEP(String orderId) async =>
     '${icorrectDomain}ai-response/index1.html?order_id=$orderId&token=${await Utils.getAccessToken()}';
 
 String specialHomeWorksEP(
     String email, String activityId, int status, int example) {
-  return "$publicDomain"
+  return "$oldPublicDomain"
       "api/list-answers-activity?activity_id="
       "$activityId"
       "&email="
@@ -64,15 +67,15 @@ String submitExam() {
 }
 
 String getTestDetailWithIdEP(String testId) =>
-    '${toolDomain}api/get-test-with-id/$testId';
+    '${oldToolDomain}api/get-test-with-id/$testId';
 
 String getActivitiesList(Map<String, String> queryParameters) {
   return '${apiDomain}api/v1/syllabus/activities-of-class/index?${Uri(queryParameters: queryParameters).query}';
 }
 
-String getUserAuthDetailEP()=>'$icorrectDomain/api/v1/exam/voice-bio/detail';
+String getUserAuthDetailEP() => '$icorrectDomain/api/v1/exam/voice-bio/detail';
 String submitAuthEP() {
-  return  '$icorrectDomain/api/v1/exam/voice-bio/submit';
+  return '$icorrectDomain/api/v1/exam/voice-bio/submit';
 }
 
 class RequestMethod {
