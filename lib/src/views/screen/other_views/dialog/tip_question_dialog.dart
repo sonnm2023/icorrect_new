782840e4c08864b5584d44b1346a6e8e454bb953
trieutoan2110 <toantrieu2110@gmail.com>
2024-09-26 11:22:36 +0700

    fix_Tip_not_scroll

--- lib/src/views/screen/other_views/dialog/tip_question_dialog.dart
+++ lib/src/views/screen/other_views/dialog/tip_question_dialog.dart
@@ -17,128 +17,130 @@ class TipQuestionDialog {
           body: Container(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      margin: const EdgeInsets.only(top: 20, right: 10),
-                      child: Text(
-                        Utils.multiLanguage(StringConstants.tips_screen_title)!,
-                        style: CustomTextStyle.textWithCustomInfo(
-                          context: context,
-                          color: Colors.orange,
-                          fontsSize: FontsSize.fontSize_20,
-                          fontWeight: FontWeight.w600,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.only(top: 20, right: 10),
+                        child: Text(
+                          Utils.multiLanguage(StringConstants.tips_screen_title)!,
+                          style: CustomTextStyle.textWithCustomInfo(
+                            context: context,
+                            color: Colors.orange,
+                            fontsSize: FontsSize.fontSize_20,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      alignment: Alignment.topRight,
-                      child: InkWell(
-                        onTap: () {
-                          Provider.of<AuthProvider>(context, listen: false)
-                              .setShowDialogWithGlobalScaffoldKey(
-                                  false, GlobalScaffoldKey.showTipScaffoldKey);
-                          Navigator.pop(context);
-                        },
-                        child: const Icon(
-                          Icons.cancel_outlined,
-                          color: Colors.black,
+                      Container(
+                        alignment: Alignment.topRight,
+                        child: InkWell(
+                          onTap: () {
+                            Provider.of<AuthProvider>(context, listen: false)
+                                .setShowDialogWithGlobalScaffoldKey(
+                                    false, GlobalScaffoldKey.showTipScaffoldKey);
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.cancel_outlined,
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  question.content.toString(),
-                  textAlign: TextAlign.center,
-                  style: CustomTextStyle.textWithCustomInfo(
-                    context: context,
-                    color: AppColor.defaultBlackColor,
-                    fontsSize: FontsSize.fontSize_18,
-                    fontWeight: FontWeight.w500,
+                      )
+                    ],
                   ),
-                ),
-                const SizedBox(height: 5),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: const Divider(
-                    thickness: 1,
-                    color: AppColor.defaultGrayColor,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    question.numPart == PartOfTest.part2.get
-                        ? Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                Utils.multiLanguage(StringConstants.cue_card)!,
-                                style: CustomTextStyle.textWithCustomInfo(
-                                  context: context,
-                                  color: AppColor.defaultBlackColor,
-                                  fontsSize: FontsSize.fontSize_16,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              Text(
-                                question.cueCard.trim(),
-                                style: CustomTextStyle.textWithCustomInfo(
-                                  context: context,
-                                  color: AppColor.defaultBlackColor,
-                                  fontsSize: FontsSize.fontSize_16,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              )
-                            ],
-                          )
-                        : Container(),
-                    const SizedBox(height: 5),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        question.numPart == PartOfTest.part2.get
-                            ? Text(
-                                Utils.multiLanguage(
-                                    StringConstants.another_tips)!,
-                                style: CustomTextStyle.textWithCustomInfo(
-                                  context: context,
-                                  color: AppColor.defaultBlackColor,
-                                  fontsSize: FontsSize.fontSize_16,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              )
-                            : Container(),
-                        (question.tips.toString().isNotEmpty)
-                            ? Text(
-                                question.tips.toString(),
-                                style: CustomTextStyle.textWithCustomInfo(
-                                  context: context,
-                                  color: AppColor.defaultBlackColor,
-                                  fontsSize: FontsSize.fontSize_16,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              )
-                            : EmptyWidget.init().buildNothingWidget(
-                                context,
-                                Utils.multiLanguage(
-                                    StringConstants.no_data_message)!,
-                                widthSize: 100,
-                                heightSize: 100,
-                              )
-                      ],
+                  const SizedBox(height: 5),
+                  Text(
+                    question.content.toString(),
+                    textAlign: TextAlign.center,
+                    style: CustomTextStyle.textWithCustomInfo(
+                      context: context,
+                      color: AppColor.defaultBlackColor,
+                      fontsSize: FontsSize.fontSize_18,
+                      fontWeight: FontWeight.w500,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    child: const Divider(
+                      thickness: 1,
+                      color: AppColor.defaultGrayColor,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      question.numPart == PartOfTest.part2.get
+                          ? Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  Utils.multiLanguage(StringConstants.cue_card)!,
+                                  style: CustomTextStyle.textWithCustomInfo(
+                                    context: context,
+                                    color: AppColor.defaultBlackColor,
+                                    fontsSize: FontsSize.fontSize_16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  question.cueCard.trim(),
+                                  style: CustomTextStyle.textWithCustomInfo(
+                                    context: context,
+                                    color: AppColor.defaultBlackColor,
+                                    fontsSize: FontsSize.fontSize_16,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                )
+                              ],
+                            )
+                          : Container(),
+                      const SizedBox(height: 5),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          question.numPart == PartOfTest.part2.get
+                              ? Text(
+                                  Utils.multiLanguage(
+                                      StringConstants.another_tips)!,
+                                  style: CustomTextStyle.textWithCustomInfo(
+                                    context: context,
+                                    color: AppColor.defaultBlackColor,
+                                    fontsSize: FontsSize.fontSize_16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                )
+                              : Container(),
+                          (question.tips.toString().isNotEmpty)
+                              ? Text(
+                                  question.tips.toString(),
+                                  style: CustomTextStyle.textWithCustomInfo(
+                                    context: context,
+                                    color: AppColor.defaultBlackColor,
+                                    fontsSize: FontsSize.fontSize_16,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                )
+                              : EmptyWidget.init().buildNothingWidget(
+                                  context,
+                                  Utils.multiLanguage(
+                                      StringConstants.no_data_message)!,
+                                  widthSize: 100,
+                                  heightSize: 100,
+                                )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
